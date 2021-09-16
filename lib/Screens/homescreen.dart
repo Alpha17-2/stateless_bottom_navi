6284fbed7cb2aba4a27292d91c29d4f6351bb355
Alpha17-2 <alphaisgod1@gmail.com>
2021-09-16 22:35:07 +0530

    Implemented the logic

--- lib/Screens/homescreen.dart
+++ lib/Screens/homescreen.dart
@@ -1,31 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:stateless_bottom_navi/Providers/screenIndexProvider.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _screenindexprovider = Provider.of<screenIndexProvider>(context);
+    int currentScreenIndex = _screenindexprovider.fetchCurrentScreenIndex;
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.shifting,
         showSelectedLabels: false,
         elevation: 1.5,
-        currentIndex: 0,
+        currentIndex: currentScreenIndex,
+        onTap: (value) => _screenindexprovider.updateScreenIndex(value),
         items: [
           BottomNavigationBarItem(
               label: '',
-              icon: Icon(Icons.home_outlined),
+              icon: Icon((currentScreenIndex==0)?Icons.home:Icons.home_outlined),
               backgroundColor: Colors.indigo // provide color to any one icon as it will overwrite the whole bottombar's color ( if provided any )
           ),
           BottomNavigationBarItem(
             label: '',
-            icon: Icon(Icons.search_outlined),
+            icon: Icon((currentScreenIndex==1)?Icons.search:Icons.search_outlined),
           ),
           BottomNavigationBarItem(
             label: '',
-            icon: Icon(Icons.favorite_outline),
+            icon: Icon((currentScreenIndex==2)?Icons.favorite:Icons.favorite_outline),
           ),
           BottomNavigationBarItem(
             label: '',
-            icon: Icon(Icons.person_outline),
+            icon: Icon((currentScreenIndex==3)?Icons.person:Icons.person_outline),
           ),
         ],
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
-
+import 'package:stateless_bottom_navi/Providers/screenIndexProvider.dart';
 import 'Screens/homescreen.dart';
+import 'package:provider/provider.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+    return MultiProvider(
+      child: MaterialApp(home: HomeScreen(),debugShowCheckedModeBanner: false,),
+      providers: [
+        ChangeNotifierProvider(create: (context) => screenIndexProvider())
+      ],
     );
   }
 }
