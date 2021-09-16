d7d86034331d6a3e6db81246e132907b189245dc
Alpha17-2 <alphaisgod1@gmail.com>
2021-09-16 20:47:53 +0530

    Implemented Bottom Navigation Bar

--- lib/Screens/homescreen.dart
+++ lib/Screens/homescreen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
-class HomeScreen extends StatelessWidget {
 
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        showSelectedLabels: false,
+        elevation: 1.5,
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(
+              label: '',
+              icon: Icon(Icons.home_outlined),
+              backgroundColor: Colors.indigo // provide color to any one icon as it will overwrite the whole bottombar's color ( if provided any )
+          ),
+          BottomNavigationBarItem(
+            label: '',
+            icon: Icon(Icons.search_outlined),
+          ),
+          BottomNavigationBarItem(
+            label: '',
+            icon: Icon(Icons.favorite_outline),
+          ),
+          BottomNavigationBarItem(
+            label: '',
+            icon: Icon(Icons.person_outline),
+          ),
+        ],
+      ),
     );
   }
 }
